8f7dd28da9153fe293935827f587ee2ae26f030f
xiaoqiyang <meetqy@qq.com>
2022-02-22 10:08:37 +0800

    test-page

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'oCrazy',
-  favicon: '/images/crazy.png',
-  logo: '/images/crazy.png',
+  favicon: '/ocrazy/images/crazy.png',
+  logo: '/ocrazy/images/crazy.png',
   outputPath: 'docs-dist',
   mode: 'site',
   publicPath: process.env.NODE_ENV === 'production' ? '/ocrazy/' : '/',
