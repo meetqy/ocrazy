32d8c9485945d513a6bcaf4c61ae4be650836252
xiaoqiyang <meetqy@qq.com>
2022-02-22 09:50:42 +0800

    test page

--- .umirc.ts
+++ .umirc.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'dumi';
 
-const NODE_ENV = process.env.NODE_ENV;
-
 export default defineConfig({
   title: 'oCrazy',
   favicon: '/images/crazy.png',
   logo: '/images/crazy.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  base: '/ocrazy',
   // more config: https://d.umijs.org/config
   extraPostCSSPlugins: [
     require('tailwindcss')({
