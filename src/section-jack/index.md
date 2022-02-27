c2f6d814997d69c9a65016c4f975d243a3b36904
xiaoqiyang <meetqy@qq.com>
2022-02-27 14:13:26 +0800

    feat: 新增section-jack

--- src/section-jack/index.md
+++ src/section-jack/index.md
@@ -17,14 +17,42 @@ group:
 import React from 'react';
 import { SectionJack } from 'ocrazy';
 
+const desc = (
+  <>
+    🚀 Endurance Fuel mixes clear with water to provide all your calories, electrolytes and
+    hydration. Simple to use with a clean, light taste. Just pour, shake and go!
+  </>
+);
+
+const title = 'Endurance Fuel';
+
+const button = (
+  <button className="px-10 tracking-widest py-3 mt-5 border-2 border-zinc-800 font-medium">
+    SHOP NOW
+  </button>
+);
+
+const backgroundImage = 'https://gitee.com/meetqy/ocrazy/raw/main/public/images/section/jack.webp';
+
 export default () => (
-  <div style={{ height: 10000 }}>
-    <SectionJack />
-    <SectionJack reverse />
-    <SectionJack />
-    <SectionJack reverse />
-    <SectionJack />
-    <SectionJack reverse />
+  <div style={{ height: 1200 }}>
+    <SectionJack
+      backgroundImage={backgroundImage}
+      title={title}
+      desc={desc}
+      button={button}
+      afterBg="linear-gradient(354deg, #99cc66 0%, #26a4ba 100%)"
+    />
+    <SectionJack
+      backgroundImage={backgroundImage}
+      title={title}
+      reverse
+      desc={desc}
+      button={button}
+      afterBg="linear-gradient(354deg, #2d2d84 0%, #2b2a29 100%)"
+    />
   </div>
 );
 ```
+
+<API></API>
--- src/section-jack/index.tsx
+++ src/section-jack/index.tsx
@@ -1,10 +1,67 @@
 import { useScroll } from 'ahooks';
+import type { CSSProperties } from 'react';
 import React, { useState } from 'react';
 import './index.less';
 
 export interface SectionJackProps {
-  reverse: boolean;
+  /**反向 */
+  reverse?: boolean;
+  /**容器最小高度 */
+  minHeight?: string;
+  /**标题 */
+  title?: string;
+  /**描述 */
+  desc?: string;
+  /**按钮 */
+  button?: JSX.Element;
+
+  /**滚动的背景图片 */
+  backgroundImage?: string;
+  /**设置var变量，会被赋值给 "--section-jack-after-bg"   */
+  afterBg?: string;
+
+  /**自定义左侧内容 */
+  leftWrapper?: JSX.Element;
+  /**自定义有侧内容 */
+  rightWrapper?: JSX.Element;
 }
+
+const defaultProps: SectionJackProps = {
+  minHeight: '650px',
+  afterBg: 'linear-gradient(354deg, #99cc66 0%, #26a4ba 100%)',
+};
+
+const leftWrapper = (props: SectionJackProps, top: number) => {
+  return props.leftWrapper ? (
+    props.leftWrapper
+  ) : (
+    <div className={`py-5 ${props.reverse ? 'lg:pl-5 pl-0' : 'pr-5'}`}>
+      <div
+        style={{
+          backgroundImage: `url(${props.backgroundImage})`,
+          backgroundSize: '100%',
+          paddingBottom: '31.4465%',
+          backgroundPositionX: top + 'px',
+        }}
+      />
+    </div>
+  );
+};
+
+const rightWrapper = (props: SectionJackProps) => {
+  return props.rightWrapper ? (
+    props.rightWrapper
+  ) : (
+    <div
+      className={`py-10 lg:px-5 max-w-3xl text-zinc-700 ${props.reverse ? 'px-5' : 'px-20'}`}
+      style={{ fontFamily: 'Gotham', color: '#543b5b' }}
+    >
+      <h2 className="text-6xl tracking-widest  font-light">{props.title}</h2>
+      <p className="text-xl pt-5 max-w-lg leading-7">{props.desc}</p>
+      {props.button}
+    </div>
+  );
+};
 
 const SectionJack = (props: SectionJackProps) => {
   const [top, setTop] = useState(0);
@@ -12,59 +69,25 @@ const SectionJack = (props: SectionJackProps) => {
   const scrllTop = scroll?.top || 0;
   setTimeout(() => setTop(props.reverse ? -scrllTop : scrllTop));
 
+  const rootVar = {
+    '--section-jack-after-bg': props.afterBg,
+  } as CSSProperties;
+
   return (
-    <section className="w-full">
-      <div className={`section-jack ${props.reverse && 'reverse'}`} style={{ minHeight: '650px' }}>
-        <div className="w-3/5">
-          <div className={`py-5 ${props.reverse ? 'pl-5' : 'pr-5'}`}>
-            <div
-              style={{
-                backgroundImage: `url(https://cdn.shopify.com/s/files/1/0248/8709/9439/files/Tailwind-Challenge_new-2_1050x.png?v=1634548088)`,
-                backgroundSize: '100%',
-                paddingBottom: '31.4465%',
-                backgroundPositionX: top + 'px',
-              }}
-            />
-          </div>
-        </div>
-        <div className="w-2/5">
-          <div
-            className="py-10 px-5 max-w-3xl text-zinc-700"
-            style={{ fontFamily: 'Gotham', color: '#543b5b' }}
-          >
-            <h4 className="text-3xl tracking-widest font-extrabold">FOR DURING</h4>
-            <h2 className="text-6xl tracking-widest pb-2 font-light">ENDURANCE FUEL</h2>
-            <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="11.685"
-                height="24.13"
-                viewBox="0 0 11.685 24.13"
-              >
-                <path
-                  id="Path_538"
-                  data-name="Path 538"
-                  d="M2302.346,508.617l8.8-12.177c.319-.481.641-.682.878-.682.326,0,.492.384.26.945l-4.419,9.613h4.9c.966,0,1.234.69.562,1.453l-9.642,11.434c-.384.489-.719.686-.951.686-.35,0-.478-.444-.21-1.007l4.209-8.8h-3.771C2302,510.084,2301.733,509.438,2302.346,508.617Z"
-                  transform="translate(-2302.018 -495.758)"
-                  fill="#543b5b"
-                />
-              </svg>
-              <span className="text-xl tracking-wider ml-3 font-extrabold">
-                CALORIES, ELECTROLYTES, HYDRATION & JOY
-              </span>
-            </p>
-            <p className="text-xl pt-5 max-w-lg leading-7">
-              Endurance Fuel mixes clear with water to provide all your calories, electrolytes and
-              hydration. Simple to use with a clean, light taste. Just pour, shake and go!
-            </p>
-            <button className="px-10 tracking-widest py-3 mt-5 border-2 border-zinc-800 font-medium">
-              SHOP NOW
-            </button>
-          </div>
-        </div>
+    <section className="w-full" style={rootVar}>
+      <div
+        className={`section-jack ${props.reverse && 'reverse'} ${
+          props.reverse ? 'lg:flex-row-reverse flex-col' : 'lg:flex-row flex-col'
+        }`}
+        style={{ minHeight: props.minHeight }}
+      >
+        <div className="lg:w-3/5 w-full">{leftWrapper(props, top)}</div>
+        <div className="lg:w-2/5 w-full">{rightWrapper(props)}</div>
       </div>
     </section>
   );
 };
+
+SectionJack.defaultProps = defaultProps;
 
 export default SectionJack;
